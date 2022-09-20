f5e0b868ab0e57b37a03d30af72f2fd4fe7265c4
Joseph Zins <jzins1@css.edu>
2022-09-20 09:20:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing Github Repo");
+        }
+    }
+}
